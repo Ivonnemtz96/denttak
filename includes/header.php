789499a66853594192f694fc744743ea6d22dedd
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-12-14 13:54:33 -0700

    header ok

--- includes/header.php
+++ includes/header.php
@@ -16,56 +16,21 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle p-0" href="#" id="navbarDropdown3" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Home
+                        <a class="nav-link p-0" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            Inicio
                         </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown3">
-                            <a class="dropdown-item" href="index.html">Home 01</a>
-                            <a class="dropdown-item active" href="index-02.html">index 02</a>
-                            <a class="dropdown-item" href="index-03.html">Home 03</a>
-                        </div>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link p-0" href="about.html">About</a>
+                        <a class="nav-link p-0" href="about.html">Clínica</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link p-0" href="services.html">Services</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle p-0" href="#" id="navbarDropdown5" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Pages
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown5">
-                            <a class="dropdown-item" href="projects.html">Projects</a>
-                            <a class="dropdown-item" href="shop.html">Shop</a>
-                            <a class="dropdown-item" href="cart.html">Cart</a>
-                            <a class="dropdown-item" href="product-detail.html">Product detail</a>
-                            <a class="dropdown-item" href="checkout.html">Checkout</a>
-                        </div>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle p-0" href="#" id="navbarDropdown4" role="button"
-                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Blogs
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown4">
-                            <a class="dropdown-item" href="load-more.html">Load More</a>
-                            <a class="dropdown-item" href="single-blog.html">Single Blog</a>
-                            <a class="dropdown-item" href="one-column.html">One Column</a>
-                            <a class="dropdown-item" href="two-column.html">Two Column</a>
-                            <a class="dropdown-item" href="three-column.html">Three Column</a>
-                            <a class="dropdown-item" href="three-colum-sidbar.html">Three Column Sidbar</a>
-                            <a class="dropdown-item" href="four-column.html">Four Column</a>
-                            <a class="dropdown-item" href="six-colum-full-wide.html">Six Column</a>
-                        </div>
+                        <a class="nav-link p-0" href="services.html">Tratamientos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link p-0" href="pricing.html">Pricing</a>
+                        <a class="nav-link p-0" href="services.html">Blog</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link p-0" href="contact.html">Contact</a>
+                        <a class="nav-link p-0" href="contact.html">Contacto</a>
                     </li>
                 </ul>
                 <div class="header-contact d-flex align-items-center">
@@ -74,19 +39,11 @@
                             <img src="assets/images/phone.png" alt="phone">
                         </figure>
                         <div class="phone-txt">
-                            <span class="d-block">Call us now:</span>
-                            <a href="tel:+0123456789">+012 (345) 6789</a>
+                            <span class="d-block">Nuestra experiencia a tu servicio:</span>
+                            <a href="tel:+526243581370">+52 624 358 1370</a>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="cart-box">
-                <a href="#">
-                    <figure class="mb-0"><img src="assets/images/search-img.png" alt="search-img"></figure>
-                </a>
-                <a href="#">
-                    <figure class="mb-0"><img src="assets/images/cart2.png" alt="cart2"></figure>
-                </a>
             </div>
         </nav>
     </div>
