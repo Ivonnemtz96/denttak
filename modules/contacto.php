07fb2bc07ea45d61d21fc6fe9fb7a67ed6d5dafc
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-12-15 15:55:59 -0700

    contacto ok

--- modules/contacto.php
+++ modules/contacto.php
@@ -1,14 +1,11 @@
 <section class="service-banner sub-banner-section w-100 float-left d-flex align-items-center">
     <div class="container">
         <div class="sub-banner-inner-con text-center">
-            <h1 data-aos="fade-up" data-aos-duration="700">Contact Us</h1>
-            <p data-aos="fade-up" data-aos-duration="700">Boidunt eget semper nec ruam sed hendrerit morbi aeu feliseao
-                augue <br>
-                pellentesue veniam morbi acer.</p>
+            <h1 data-aos="fade-up" data-aos-duration="700">Contáctanos</h1>
             <nav aria-label="breadcrumb" data-aos="fade-up" data-aos-duration="700">
                 <ol class="breadcrumb d-inline-block mb-0">
-                    <li class="breadcrumb-item d-inline-block"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item active text-white d-inline-block" aria-current="page">Contact</li>
+                    <li class="breadcrumb-item d-inline-block"><a href="index.php">Inicio</a></li>
+                    <li class="breadcrumb-item active text-white d-inline-block" aria-current="page">Contacto</li>
                 </ol>
             </nav>
         </div>
@@ -25,7 +22,7 @@
                         <img src="assets/images/location-img.png" alt="location-img">
                     </figure>
                     <div class="contact-txt">
-                        <h4>Location:</h4>
+                        <h4>Dirección:</h4>
                         <p class="mb-0">121 King Street, Melbourne
                             Victoria 3000 Australia</p>
                     </div>
@@ -35,9 +32,8 @@
                         <img src="assets/images/phone-img.png" alt="phone-img">
                     </figure>
                     <div class="contact-txt">
-                        <h4>Phone:</h4>
+                        <h4>Teléfono:</h4>
                         <a href="tel:+61383766284">(+61 3 8376 6284)</a>
-                        <a href="tel:+80023456789">(+800 2345 6789)</a>
                     </div>
                 </div>
                 <div class="contact-detail" data-aos="fade-up" data-aos-duration="700">
@@ -45,43 +41,44 @@
                         <img src="assets/images/mail-img.png" alt="mail-img">
                     </figure>
                     <div class="contact-txt">
-                        <h4>Email us at:</h4>
-                        <a href="mailto:info@medunit.com">info@medunit.com</a>
-                        <a href="mailto:medunit@gmail.com">medunit@gmail.com</a>
+                        <h4>Correo:</h4>
+                        <a href="mailto:info@denttakclinica.com">info@denttakclinica.com</a>
                     </div>
                 </div>
             </div>
             <div class="contact-right-sec">
                 <div class="contact-form ">
-                    <span class="sub-txt d-block" data-aos="fade-up" data-aos-duration="700">Get in Touch</span>
-                    <h2 data-aos="fade-up" data-aos-duration="700">Send us a Message</h2>
-                    <p data-aos="fade-up" data-aos-duration="700">Molestiae non recusandae itaque earum rerum sarien.
+                    <span class="sub-txt d-block" data-aos="fade-up" data-aos-duration="700">Estamos en contacto
+                        contigo</span>
+                    <h2 data-aos="fade-up" data-aos-duration="700">Mándanos mensaje</h2>
+                    <p data-aos="fade-up" data-aos-duration="700">
+                        Uno de nuestros especialistas se pondrá en contacto contigo.
                     </p>
                     <form class="form-box" method="post" id="contactpage">
                         <ul class="list-unstyled ">
                             <li data-aos="fade-up" data-aos-duration="700">
-                                <input type="text" name="fname" id="fname" placeholder="First name:">
+                                <input type="text" name="fname" id="fname" placeholder="Nombre:">
                                 <i class="fas fa-user"></i>
                             </li>
                             <li data-aos="fade-up" data-aos-duration="700">
-                                <input type="text" name="lname" id="lname" placeholder="Last name:">
+                                <input type="text" name="lname" id="lname" placeholder="Apellido:">
                                 <i class="fas fa-user"></i>
                             </li>
                             <li data-aos="fade-up" data-aos-duration="700">
-                                <input type="email" placeholder="Email address:" name="email" id="email">
+                                <input type="email" placeholder="Correo:" name="email" id="email">
                                 <i class="fas fa-envelope"></i>
                             </li>
                             <li data-aos="fade-up" data-aos-duration="700">
-                                <input type="tel" name="phone" id="phone" placeholder="Phone number">
+                                <input type="tel" name="phone" id="phone" placeholder="Teléfono:">
                                 <i class="fas fa-phone-alt"></i>
                             </li>
                             <li data-aos="fade-up" data-aos-duration="700">
-                                <textarea placeholder="Message" name="subject" id="subject"></textarea>
+                                <textarea placeholder="Mensaje" name="subject" id="subject"></textarea>
                             </li>
                         </ul>
                         <div class="submit-btn" data-aos="fade-up" data-aos-duration="700">
                             <button type="submit" id="submit">
-                                Send Now
+                                Enviar
                             </button>
                         </div>
                     </form>
@@ -94,7 +91,7 @@
 <!-- MAP SECTION START -->
 <div class="index2-map-section w-100 float-left">
     <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d331.28489877721745!2d144.95688568074652!3d-37.81977238709575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d52754eaecb%3A0x22f367daf52cbd47!2s21%20King%20St%2C%20Melbourne%20VIC%203000%2C%20Australia!5e0!3m2!1sen!2s!4v1696843179791!5m2!1sen!2s"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1821.6724334376836!2d-109.7258643524875!3d23.106388680853627!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86af5be0d52cea3d%3A0xbe564dda6c9115d4!2sDenttak%20Cl%C3%ADnica%20Dental!5e0!3m2!1ses-419!2smx!4v1702680920479!5m2!1ses-419!2smx"
         width="600" height="578" style="border:0;" allowfullscreen="" loading="lazy"
         referrerpolicy="no-referrer-when-downgrade"></iframe>
 </div>
