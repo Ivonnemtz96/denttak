a80080fd450782e636f551054226e4d62faab495
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-12-15 13:40:51 -0700

    clínica ok

--- modules/clinica.php
+++ modules/clinica.php
@@ -1,14 +1,11 @@
 <section class="sub-banner-section w-100 float-left d-flex align-items-center">
     <div class="container">
         <div class="sub-banner-inner-con text-center">
-            <h1 data-aos="fade-up" data-aos-duration="700">About us</h1>
-            <p data-aos="fade-up" data-aos-duration="700">Roidunt eget semper nec ruam sed hendrerit morbi aeu
-                feliseao augue <br>
-                pellentesue veniam morbi acer.</p>
+            <h1 data-aos="fade-up" data-aos-duration="700">Denttak Clínica Dental</h1>
             <nav aria-label="breadcrumb" data-aos="fade-up" data-aos-duration="700">
                 <ol class="breadcrumb d-inline-block mb-0">
-                    <li class="breadcrumb-item d-inline-block"><a href="index.html">Home</a></li>
-                    <li class="breadcrumb-item active text-white d-inline-block" aria-current="page">About</li>
+                    <li class="breadcrumb-item d-inline-block"><a href="index.php">Inicio</a></li>
+                    <li class="breadcrumb-item active text-white d-inline-block" aria-current="page">Nosotros</li>
                 </ol>
             </nav>
         </div>
@@ -26,34 +23,82 @@
             </div>
             <div class="about-txt-con">
                 <div class="generic-title" data-aos="fade-up" data-aos-duration="700">
-                    <span class="small-text">About Us</span>
-                    <h2 class="mb-0">Therapy for How We Can
-                        Live Better Today</h2>
+                    <span class="small-text">¿Quiénes somos?</span>
+                    <h2 class="mb-0">Denttak Clínica Dental</h2>
                 </div>
-                <p data-aos="fade-up" data-aos-duration="700">Quis nostrud exercitation ullamco laboris nisi aut
-                    aliquio
-                    modo consequat ruis aute irure dolor in reprehenderit in
-                    voluptate velit esse cillum dolore.</p>
-                <p data-aos="fade-up" data-aos-duration="700">Wiusmod tempor incididunt ut labore et dolore magna
-                    aliqua zut enim ad minim.</p>
+                <p data-aos="fade-up" data-aos-duration="700">
+                    La ortodoncia es uno de los métodos más populares
+                    para corregir los dientes y conseguir una sonrisa bella
+                    y armónica. Los tratamientos más conocidos son los
+                    que están compuestos de brackets (brackets
+                    metálicos, estéticos y autoligado). <br>
+                    Mediante la
+                    aplicación de fuerzas muy suaves a través de un arco,
+                    los brackets o aparatos fijos funcionan moviendo tus
+                    dientes lentamente hasta colocarlos en la posición
+                    final adecuada.
+                </p>
                 <ul class="list-unstyled" data-aos="fade-up" data-aos-duration="700">
-                    <li>Psychodynamic Therapy</li>
-                    <li>Free Consultants</li>
-                    <li>Couple Problem Therapy</li>
-                    <li>Metal Satisfications</li>
+                    <li>Equipos Modernos</li>
+                    <li>Personal Certificado</li>
+                    <li>Años de experiencia</li>
                 </ul>
                 <div class="generic-btn" data-aos="fade-up" data-aos-duration="700">
-                    <a href="services.html">Get Started</a>
+                    <a href="contacto.php">Agendar cita</a>
                 </div>
             </div>
         </div>
-        <div class="about-special-txt" data-aos="fade-up" data-aos-duration="700">
-            <figure>
-                <img src="assets/images/about-quote-img.png" alt="about-quote-img">
-            </figure>
-            <h4>Ignissimos ducimus aui
-                aet quas molestias exceuri
-                recusandae itarue.</h4>
+    </div>
+</section>
+
+<!-- CONTACT SECTION START -->
+<section id="index2-cntct" class="w-100 float-left index2-contact-sec contact-con position-relative">
+    <div class="padding-top padding-bottom contact-inner-con position-relative">
+        <div class="container">
+            <div class="contact-form ">
+                <span class="sub-txt d-block" data-aos="fade-up" data-aos-duration="700">¡Agenda tu cita
+                    ahora!</span>
+                <h2 data-aos="fade-up" data-aos-duration="700">Denttak</h2>
+                <p data-aos="fade-up" data-aos-duration="700">Uno de nuestros asesores se pondrá en contacto contigo
+                </p>
+                <form class="form-box" method="post" id="contactpage">
+                    <ul class="list-unstyled ">
+                        <li data-aos="fade-up" data-aos-duration="700">
+                            <input type="text" name="fname" id="fname" placeholder="Nombre:">
+                            <i class="fas fa-user"></i>
+                        </li>
+                        <li data-aos="fade-up" data-aos-duration="700">
+                            <input type="text" name="fname" id="fname" placeholder="Apellido:">
+                            <i class="fas fa-user"></i>
+                        </li>
+                        <li data-aos="fade-up" data-aos-duration="700">
+                            <input type="email" placeholder="Correo:" name="email" id="email">
+                            <i class="fas fa-envelope"></i>
+                        </li>
+                        <li data-aos="fade-up" data-aos-duration="700">
+                            <input type="tel" name="phone" id="phone" placeholder="Teléfono">
+                            <i class="fas fa-phone-alt"></i>
+                        </li>
+                        <li data-aos="fade-up" data-aos-duration="700">
+                            <select name="department" id="department">
+                                <option value="department">Sucursal</option>
+                                <option value="department1">San José del Cabo</option>
+                                <option value="department2">Cabo San Lucas</option>
+                            </select>
+                        </li>
+                        <li data-aos="fade-up" data-aos-duration="700">
+                            <input placeholder="Date & Time" type="date" id="date">
+                        </li>
+
+                    </ul>
+                    <div class="submit-btn" data-aos="fade-up" data-aos-duration="700">
+                        <button type="submit" id="submit">
+                            Revisar disponibilidad
+                        </button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </section>
+<!-- CONTACT SECTION END -->
